d843eda3e873606df99f9298ebe3afe7aeeb0caa
tanu <tanushreehirwane@gmail.com>
2022-10-13 01:16:35 +0530

    ohkyyyyy

--- src/components/HighOrderComp.jsx
+++ src/components/HighOrderComp.jsx
@@ -5,6 +5,7 @@ function HighOrderComp() {
     <div>
         <h1 className='App'>High Order Component</h1>
         <ul>
+            <li>HOCs basically incorporate the don’t-repeat-yourself (DRY) principle of programming, which you’ve most likely come across at some point in your career as a software developer. It is one of the best-known principles of software development, and observing it is very important when building an application or writing code in general.</li>
             <li>The Higher-Order component is simply called HOC.</li>
             <li>A pattern where a function takes a component as an argument and returns a new component.</li>
            
